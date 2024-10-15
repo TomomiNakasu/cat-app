ec5ce2495cc7924b97523cde90568d2d98bc00f7
tomominakasu@gmail.com <tomominakasu@gmail.com>
2024-10-15 11:36:46 +0800

    display cats name

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+
 import './App.css'
 
+interface ICat {
+  cat_id: number;
+  cat_name: string;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [cats, setCats] = useState<ICat[] | undefined>(undefined);
+
+  const URL_GET = 'https://cat-backend-ten.vercel.app/api/cats';
+  
+  useEffect(() => {
+    fetch(URL_GET)
+      .then((response) => response.json())
+      .then((cats) => {
+      
+        setCats(cats)
+      })
+  }, []);
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+        {cats !== undefined && cats.map((cat) => (
+          <p key={cat.cat_id}>{cat.cat_name}</p>
+        ))}
+    </div>
   )
 }
 
